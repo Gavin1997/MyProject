a5d2978b64c8b123198cc5f4436f343bb18ff7f2
Gavin1997 <15108466849@163.com>
2018-10-10 01:10:23 +0800

    将产品搜索页分页功能完善,在路由中增加了用户注册功能,并用正则验证

--- 02_travel/public/JS/products.js
+++ 02_travel/public/JS/products.js
@@ -22,7 +22,8 @@ new Vue({
             n:0,
             disabled_info_prev:"disabled",
             disabled_info_next:"btn_area_active",
-            disabled_info_num:""
+            disabled_info_num:"",
+            changeActive:false,
         }
     },
     methods:{
@@ -34,47 +35,58 @@ new Vue({
                 console.log(this.kw)
                 this.res=res.data.data.products;
                 this.pageCount=res.data.data.pageCount;
-                // console.log(res.data.data);
+                //  console.log(res.data.data);
+                 this.pno=res.data.data.pno;
                 // console.log(this.pageCount)
                 
                 
             })
         },
-        next(i){
-            if(this.disabled_info_next!="disabled"){
+        next(){
+            console.log();
                if(this.pno<this.pageCount-1){
                    this.pno++;
+                   this.n++;
                    this.disabled_info_next="btn_area_active";
                    this.disabled_info_prev="btn_area_active";
-                   this.getMsg()
-                  
+                   this.getMsg();         
                }else{
                    this.disabled_info_next="disabled"
                }
-            }
+            
         },
         prev(){
             if(this.disabled_info_prev!="disabled"){
                 if(this.pno<this.pageCount &&this.pno>0){
                     this.pno--;
+                    this.n--;
                     this.getMsg();
                     this.disabled_info_next="btn_area_active";
                     this.disabled_info_prev="btn_area_active";
                 }else{
                     this.disabled_info_prev="disabled";
                 }
-
-
             }
         },
         nowNum(i){
-            this.pno=i
-            this.getMsg();
-            console.log(i)
-           
+            this.pno=i;
+            this.changeActive=i;
+            this.getMsg();  
+            this.disabled_info_prev="btn_area_active";
+            this.disabled_info_next="btn_area_active";
+        },
+    },
+    watch:{
+        pno(){
+            if(this.pno=this.n){
+                this.changeActive = this.n
+            }else{
+                this.changeActive!=this.n;
+            }
+            console.log(`当前pno的值${this.pno}`)
+            console.log(this.n);
         }
     },
-    watch:{},
     created() {
        this.getMsg();
     },
--- 02_travel/routes/users.js
+++ 02_travel/routes/users.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const pool=require("../pool");
-
+//登录功能
 router.get("/signin",(req,res)=>{
     var {uname,upwd}=req.query;
     var sql="select *from qy_user where uname=? and upwd=?";
@@ -22,5 +22,48 @@ router.get("/signin",(req,res)=>{
         res.end();
        
     })
-})
+});
+//注册功能
+router.get("/register",(req,res)=>{
+    var uid = req.query.uid;
+    var uname=req.query.uname;
+    var upwd= req.query.upwd;
+    var email=req.query.email;
+    var phone=req.query.phone;
+    var user_name=req.query.user_name;
+    var gender=req.query.gender;
+    if(!uname){
+        res.send({code:-1,msg:"用户名不能为空哦,请检查后再输入哦"})
+        return
+    };
+    var reg=/^(?![a-z0-9]+$)(?![A-Za-z]+$)[A-Za-z0-9]{6,8}$///6-8位数字,字母结合,至少包含一个大写字母和数字;
+    if(!upwd ||!reg.test(upwd)){
+        res.send({code:-1,msg:"密码格式不正确,请检查后再输入哦"})
+        return
+    };
+    if(!email){
+        res.send({code:-1,msg:"邮箱格式不正确,请检查后再输入哦"})
+        return
+    };
+    var reg1=/^[1][3578][0-9]{9}$/
+    if(!phone || !reg1.test(phone)){
+        res.send({code:-1,msg:"手机号码格式不正确,请检查后再输入"})
+        return
+    };
+    if(!user_name){
+        res.send({code:-1,msg:"用户姓名不能为空,请检查后再输入"})
+        return
+    };
+    if(!gender){
+        res.send({code:-1,msg:"用户性别不能为空,请检查后重新输入哦"})
+        return
+    };
+    var sql = "INSERT INTO `qy_user`(`uid`, `uname`, `upwd`, `email`, `phone`, `user_name`, `gender`) VALUES (null,?,?,?,?,?,?)";
+    pool.query(sql,[uid,uname,upwd,email,phone,user_name,gender],(err,result)=>{
+        if(err) throw err;
+        if(affectedRows=1){
+          res.send({code:1,msg:`注册成功,欢迎${uname}!!`})
+        }
+    })
+});
 module.exports=router;
